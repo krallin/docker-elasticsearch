9d55bdc11bd32687f9d238936492a55480f6a8cc
Thomas Orozco <thomas@orozco.fr>
2016-02-24 10:11:39 +0100

    Verify SHA1 sums of downloaded archives

--- 1.5/config.mk
+++ 1.5/config.mk
@@ -1,4 +1,5 @@
 export ES_VERSION = 1.5.2
+export ES_SHA1SUM = ffe2e46ec88f4455323112a556adaaa085669d13
 export ES_BACKUP_PLUGIN = elasticsearch/elasticsearch-cloud-aws/2.5.1
 export ES_USER = root
 export ES_GROUP = root
--- 2.2/config.mk
+++ 2.2/config.mk
@@ -1,4 +1,5 @@
 export ES_VERSION = 2.2.0
+export ES_SHA1SUM = 4bd3ef681e70faefe3a66c6eb3419b5d4a0e2714
 export ES_BACKUP_PLUGIN = cloud-aws
 export ES_USER = elasticsearch
 export ES_GROUP = elasticsearch
